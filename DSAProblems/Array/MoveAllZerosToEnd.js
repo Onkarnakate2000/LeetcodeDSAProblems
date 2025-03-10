b04b9d84905e04a53082c738050dfb9ae8b6cec1
Onkar Nakate <119847324+Onkarnakate2000@users.noreply.github.com>
2025-03-10 06:28:48 +0000

    solved allZerosMoveToEnd

---
+++ DSAProblems/Array/MoveAllZerosToEnd.js
@@ -0,0 +1,55 @@
+// function moveZeroesToEnd(arr){
+//     let n = arr.length;
+//     let sortedArray=[];
+//     sortedArray = arr.filter((item)=>{
+//         if(item===0){
+//             sortedArray.splice(n,0,0)
+//         }else{
+//             sortedArray.push(item)
+//         }
+//     })
+    
+//     console.log("sortedArray :",sortedArray);
+// }
+
+// moveZeroesToEnd([2,3,5,7,0,3,2,0,5])
+
+////////////////////////////////
+
+// function moveZeroesToEnd(arr){
+//     let n = arr.length;
+//     let index=0;
+//     for(let i=0;i<n;i++){
+//         if (arr[i] !== 0) {
+//             // console.log(arr[index], arr[i])
+//             [arr[index], arr[i]] = [arr[i], arr[index]]; // Swap
+//             index++;
+//         }
+//     }
+//     console.log("sortedArray:", arr);
+
+// }
+
+// moveZeroesToEnd([2,3,5,7,0,3,2,0,5])
+
+////////////////////////////////
+
+
+function moveZeroesToEnd(arr){
+    let n = arr.length;
+    let temp = new Array(n);
+    let j=0;
+    for(let i=0;i<n;i++){
+        if(arr[i]!== 0){
+            temp[j++] = arr[i];
+        }
+    }
+    while(j<n){
+        temp[j++] = 0;
+    }
+    for(let i=0;i<n;i++){
+        arr[i]=temp[i]  
+    }
+}
+
+moveZeroesToEnd([2,3,5,7,0,3,2,0,5])
